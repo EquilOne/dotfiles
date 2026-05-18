b630ed65133658c512ff1ab3d31bc34b1c992b6b
Chase <188378569+EquilOne@users.noreply.github.com>
2026-05-18 09:16:22 -0600

    Add .config/omarchy/current/theme/README.md Add .config/omarchy/current/theme/alacritty.toml Add .config/omarchy/current/theme/assets/palette.webp Add .config/omarchy/current/theme/assets/pallete-dawn.webp Add .config/omarchy/current/theme/assets/tools-dawn.webp Add .config/omarchy/current/theme/assets/tools.webp Add .config/omarchy/current/theme/assets/wallpaper-dawn.webp Add .config/omarchy/current/theme/assets/wallpaper.webp Add .config/omarchy/current/theme/backgrounds/1-background.png Add .config/omarchy/current/theme/backgrounds/2-background.png Add .config/omarchy/current/theme/backgrounds/2404320.jpg Add .config/omarchy/current/theme/backgrounds/3-background.png Add .config/omarchy/current/theme/backgrounds/4-background.png Add .config/omarchy/current/theme/btop.theme Add .config/omarchy/current/theme/ghostty.conf Add .config/omarchy/current/theme/hyprland.conf Add .config/omarchy/current/theme/hyprlock.conf Add .config/omarchy/current/theme/kitty.conf Add .config/omarchy/current/theme/mako.ini Add .config/omarchy/current/theme/neovim.lua Add .config/omarchy/current/theme/swayosd.css Add .config/omarchy/current/theme/walker.css Add .config/omarchy/current/theme/waybar.css

---
+++ dot_config/omarchy/current/theme/neovim.lua
@@ -0,0 +1,24 @@
+return {
+	-- Configure Rose Pine
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			variant = "moon",
+			styles = {
+				transparency = true,
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	-- Set Rose Pine as the default colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine-moon",
+		},
+	},
+}
