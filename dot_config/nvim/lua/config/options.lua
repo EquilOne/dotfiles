7a4ac40d0af0ad596aefbd278bfb92a33a5b3075
Chase <188378569+EquilOne@users.noreply.github.com>
2026-06-18 12:36:59 -0600

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,13 +3,13 @@
 -- Add any additional options here
 vim.opt.relativenumber = true
 vim.opt.mousescroll = "ver:1,hor:1"
-vim.opt.textwidth = 80
+vim.opt.textwidth = 0
 vim.opt.colorcolumn = "+1"
 vim.opt.formatoptions:remove("t")
 
 vim.api.nvim_create_autocmd("FileType", {
+  -- markdown excluded: wrapping handled by prettier + markdownlint-cli2 via vault config
   pattern = {
-    "markdown",
     "text",
     "rst",
     "tex",
